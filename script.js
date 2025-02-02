d1d8cca594f4d38e358c4408b180b800031b4799
Mahri <160527770+MahriAt@users.noreply.github.com>
2025-02-02 23:52:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const APILINK ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1";
+const APILINK ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cf551649a380289863a326f7ed92f90d&page=1";
 const IMG_PATH ="https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=cf551649a380289863a326f7ed92f90d&query=";
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
