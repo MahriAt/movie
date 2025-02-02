c8458c05693bd5acd264ecd7c682576790da1467
Mahri <160527770+MahriAt@users.noreply.github.com>
2025-02-02 18:58:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const APILINK ="https://api.themoviedb.org/3/discover/movie?include_adult=false&
 const IMG_PATH ="https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=cf551649a380289863a326f7ed92f90d&query=";
 
-const main = document.getElementById ("section");
-const form = document.getElementById ("form");
-const search = document.getElementById ("query");
+const main = document.getElementById ("#section");
+const form = document.getElementById ("#form");
+const search = document.getElementById ("#query");
 
 returnMovies(APILINK);
 function returnMovies (url){
 	fetch(url).then(res => res.json())
 	.then(function(data){
 		console.log(data.results);
-		data.results.forEach(element =>{
+		data.results.forEach(element => {
 			const div_card = document.createElement('div');
 			div_card.setAttribute('class', 'card');
 			const div_row = document.createElement('div');
